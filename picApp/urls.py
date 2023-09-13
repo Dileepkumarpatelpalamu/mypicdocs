f9237823a2dd58c1077eb779f66a92649d8d5cfe
Dileep Kumar Patel <pateldileep51@gmail.com>
2023-09-14 01:05:37 +0530

    Update urls.py,views.py and templates/index.html

--- picApp/urls.py
+++ picApp/urls.py
@@ -5,5 +5,4 @@ from django.conf.urls.static import static
 from picApp import views
 urlpatterns = [
     path('', views.Home.as_view(),name='home'),
-    path('login/', views.login,name='login'),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- picApp/views.py
+++ picApp/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from .models import ImageUpload
 from django.views import View
+from os import getcwd
+from json import load,JSONDecodeError
 # Create your views here.
-
 class Home(View):
     def get(self, request,*args,**kwargs):
+        configs= read_json(getcwd()+"/picApp/configs.json")
         context = {}
         if 'searchText' in request.session and (request.session['searchText'] !="" or request.session['searchText'] is None) :
             search_text = request.session.get('searchText')
@@ -12,9 +14,11 @@ class Home(View):
         else:
             image_data = ImageUpload.objects.all().values()
         context['image_data'] = image_data
+        context['configs'] = configs
         return render(request, 'index.html', context)
     def post(self,request,*args,**kwargs):
         context = {}
+        configs= read_json(getcwd()+"/picApp/configs.json")
         search_text = request.POST.get('search')
         if request.POST.get('btncancel') == 'cancel':
             search_text = ""
@@ -26,10 +30,15 @@ class Home(View):
             request.session['searchText'] = search_text
             image_data = ImageUpload.objects.all().values()
         context['image_data'] = image_data
+        context['configs'] = configs
         return render(request,'index.html',context)
-def login(request):
-    return render(request,'login.html')
-def signup(request):
-    return render(request,'signup.html')
-def contact(request):
-    return render(request,'contact.html')
+def read_json(path):
+    try:
+        with open(path) as json_obj:
+            data = load(json_obj)
+        return data
+    except FileNotFoundError:
+        return None
+    except JSONDecodeError as e:
+        print(f"Error parsing JSON: {e}")
+        return None
